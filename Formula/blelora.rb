e818e1aeea6d563d4abf062c8da2dfa4d7fc2989
Kent Williams <k3nt00@gmail.com>
2022-03-11 10:51:37 -0800

    add aarch64 darwin

--- Formula/blelora.rb
+++ Formula/blelora.rb
@@ -4,8 +4,13 @@ class Blelora < Formula
     homepage "https://github.com/blelora/blelora-cli"
 
     if OS.mac?
-      url "https://github.com/blelora/blelora-cli/releases/download/#{version}/blelora-cli_#{version}_x86_64-apple-darwin.zip"
-      sha256 "1aad79892e3e55761b21ea0d403e31e29b553366fc58c46d2fd02213ed85d445" 
+      if Hardware::CPU.arm?
+        url "https://github.com/blelora/blelora-cli/releases/download/#{version}/blelora-cli_#{version}_aarch64-apple-darwin.zip"
+        sha256 "34e2d0d56414af380239d6a27deb9e6ec3b3e61e2b2c5c67191e0109cf3115a6" 
+      else
+        url "https://github.com/blelora/blelora-cli/releases/download/#{version}/blelora-cli_#{version}_x86_64-apple-darwin.zip"
+        sha256 "1aad79892e3e55761b21ea0d403e31e29b553366fc58c46d2fd02213ed85d445" 
+      end
     end
   
     def install
