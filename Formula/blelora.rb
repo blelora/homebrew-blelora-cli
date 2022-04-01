0de6db6cceb1fc5cda1e62f30ccad6356be153a2
Kent Williams <k3nt00@gmail.com>
2022-04-01 14:16:47 -0700

    update for 0.1.2 release

--- Formula/blelora.rb
+++ Formula/blelora.rb
@@ -1,15 +1,15 @@
 class Blelora < Formula
-    version "0.1.1"
+    version "0.1.2"
     desc "A command-line utility written in Rust that supports Device Firmware Updates (DFU) for nRF52 devices running Adafruit_nRF52_Bootloader."
     homepage "https://github.com/blelora/blelora-cli"
 
     if OS.mac?
       if Hardware::CPU.arm?
         url "https://github.com/blelora/blelora-cli/releases/download/#{version}/blelora-cli_#{version}_aarch64-apple-darwin.zip"
-        sha256 "eb1c46419f79fc7ecbccdc4cadfccd3c7b424b982807d6a86d659a00588962d0" 
+        sha256 "cc29508802906d44990d6289df5625953c6480c59992993e140b1d6a1b4c84a1" 
       else
         url "https://github.com/blelora/blelora-cli/releases/download/#{version}/blelora-cli_#{version}_x86_64-apple-darwin.zip"
-        sha256 "d430bae945a1c0a47e01831f866498763dbbf6c59a1036efe9fa6853821ff786" 
+        sha256 "d3e478f43dd69ed04c8ed47ea3b42659324ccc7ab3c3d4876edd6e0f8219b6cc" 
       end
     end
   
